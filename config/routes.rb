aecfb0517b87b40bc7b34b84b3db70ddc4e696e0
Sulman Baig <sulman1990@hotmail.com>
2015-06-12 11:43:42 +0200

    adding name to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612075301) do
+ActiveRecord::Schema.define(version: 20150612090356) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150612075301) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "name",                   default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
