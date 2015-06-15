29528001afe4f8ac18550ab4cc9fb1a881340e21
Sulman Baig <sulman1990@hotmail.com>
2015-06-15 11:41:15 +0200

    tried to change the current password for user that comes from social network

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,4 +10,13 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :facebook, :twitter, :github, :portfolio)
   end
 
+  def update_resource(resource, params)
+    if current_user.provider == "facebook" || current_user.provider == "google_oauth2" || current_user.provider == "github"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
+
 end
